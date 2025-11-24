5cb7d9df240317ec4562972b32c37715c81cf070
obezeq <obezequiel@iesrafaelalberti.es>
2025-11-24 19:34:08 +0100

    Adding ErrorResponse Exception

---
+++ src/main/java/org/example/apirest/dto/error/ErrorResponse.java
@@ -0,0 +1,33 @@
+package org.example.apirest.dto.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private List<ValidationError> validationErrors;
+
+    @Data
+    @Builder
+    public static class ValidationError {
+        private String field;
+        private String message;
+        private Object rejectedValue;
+    }
+
+}
